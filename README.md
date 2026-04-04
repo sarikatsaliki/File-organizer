e1949ff4214933a4c6c3c67defb52236ad93e359
Sarika Tsaliki <sarikatsaliki09@gmail.com>
2026-04-04 16:14:15 +0530

    feat: initial file organizer with os and shutil logic

---
+++ README.md
@@ -0,0 +1,11 @@
+# File Flow Optimizer 🚀
+A Python-based automation tool to organize messy directories into categorized folders.
+
+## Features
+- Scans directories for specific file extensions.
+- Automatically creates category folders (Images, Docs, etc.).
+- Prevents file overwriting with existence checks.
+
+## Tech Stack
+- Python 3.x
+- `os` and `shutil` libraries
---
+++ main.py
@@ -0,0 +1,34 @@
+import os
+import shutil  # <-- 1. Add this (it stands for 'shell utilities')
+
+folder_path = "test_folder"
+files = os.listdir(folder_path)
+
+for file in files:
+    # This line prevents the script from trying to "move" a folder into itself
+    if os.path.isdir(os.path.join(folder_path, file)):
+        continue
+
+    name, extension = os.path.splitext(file)
+    ext = extension.lower()
+
+    # 2. Decide the folder name
+    if ext in [".jpg", ".png", ".jpeg"]:
+        target = "Images"
+    elif ext in [".txt", ".pdf", ".docx"]:
+        target = "Documents"
+    elif ext == ".mp3":
+        target = "Audio"
+    else:
+        target = "Others"
+
+    # 3. Create the folder if it doesn't exist
+    target_dir = os.path.join(folder_path, target)
+    if not os.path.exists(target_dir):
+        os.makedirs(target_dir)
+
+    # 4. MOVE THE FILE
+    # (Move from 'test_folder/file' to 'test_folder/target/file')
+    shutil.move(os.path.join(folder_path, file), os.path.join(target_dir, file))
+    
+    print(f"Moved {file} -> {target}")
